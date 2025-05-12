e8747b38fa3e6031223eade713b51d50c0f15c72
VAKHULA <andrii.vakhula@gmail.com>
2025-05-12 22:51:20 +0200

    revert output

--- utils/converter.ts
+++ utils/converter.ts
@@ -5,14 +5,16 @@ export const generatePage = (
 ): string => {
   let state = BigInt(page);
   let output = '';
+  let isValuable = true
 
   const length = BigInt(characterSet.length);
 
   for (let i = 0; i < numCharsPerPage; i++) {
     const operation: bigint = state % length;
 
-    output = characterSet[Number(operation.toString())] + output;
+    output =isValuable ?  output + characterSet[Number(operation.toString())] : characterSet[Number(operation.toString())] + output
     state = state / length; // it should be Math.floor(state / length) if number
+    isValuable = state === BigInt(0)
   }
 
   return output;
